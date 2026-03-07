310fa4d170f6d0a837ccb8bea07873775b672780
Muhammad-Hazimi-Yusri <muhammadhazimiyusri@gmail.com>
2026-03-07 13:53:10 +0000

    vercel: optimise image transformation using longer Cache TTL

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    minimumCacheTTL: 2678400, // 31 days
+    formats: ["image/webp"],
+    deviceSizes: [640, 828, 1200],
+    imageSizes: [32, 64, 128, 256],
     remotePatterns: [
       {
         protocol: "https",
